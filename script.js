e3f7c3c45a1548a3df69c9e6213536c656ad8fdf
budsterblue <budsterblue@gmail.com>
2019-11-24 12:41:38 -0600

    Lots of css to replicate mobile, better variables, and version bump

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function onloadcheck() {
 if (location.pathname == "/") {
 var courseGradesNum = [];
 var courseGradesID = [];
-var canvasURL = location.protocol+'//'+location.host+location.pathname + 'api/v1/courses?include[]=total_scores&per_page=100&enrollment_state=active'; 
+
 //Get Grades
-fetch(canvasURL,{
+fetch(location.protocol+'//'+location.host+location.pathname + 'api/v1/courses?include[]=total_scores&per_page=100&enrollment_state=active',{
           method: 'GET',
           credentials: 'include',
           headers: {
@@ -14,9 +14,16 @@ fetch(canvasURL,{
     .then(res => res.json())
     .then(data => obj = data)
     .then(function(){
+
 for(var i=0, len=obj.length; i<len; i++) {
-    courseGradesNum.push(obj[i].enrollments[obj[i].enrollments.length -1].computed_current_score);
-    courseGradesID.push(obj[i].name);
+  var courseEnrollment = obj[i].enrollments[obj[i].enrollments.length -1].computed_current_score;
+  if (courseEnrollment == null){
+    courseEnrollment = "N/A";
+  } else {
+    courseEnrollment += "%";
+  }
+  courseGradesNum.push(courseEnrollment);
+  courseGradesID.push(obj[i].name);
 }
 
 //Dashboard Stuff
@@ -26,11 +33,15 @@ for(var b=0, leng=courseBase.length; b<leng; b++) {
   for(var a=0, lena=courseGradesID.length; a<lena; a++) {
     if (courseName.includes(courseGradesID[a]) == true) {
       var canvasTitle = document.createElement("h1");
-      canvasTitle.style.color = "white";
-      canvasTitle.style.lineHeight = "146px";
-      canvasTitle.style.textAlign = "center";
-      canvasTitle.style.fontSize = "250%";
-      var canvasTitleText = document.createTextNode(courseGradesNum[a] + "%");
+      canvasTitle.style.color = document.getElementsByClassName('ic-DashboardCard__header_hero')[b].style.backgroundColor;
+      canvasTitle.style.background = "white";
+      canvasTitle.style.borderRadius = "1em";
+      canvasTitle.style.display = "inline";
+      canvasTitle.style.padding = "5px 15px 5px 15px";
+      canvasTitle.style.lineHeight = "65px";
+      canvasTitle.style.marginLeft = "10px";
+      canvasTitle.style.fontSize = "150%";
+      var canvasTitleText = document.createTextNode(courseGradesNum[a]);
       canvasTitle.appendChild(canvasTitleText);
       document.getElementsByClassName('ic-DashboardCard__header_hero')[b].appendChild(canvasTitle);
     }
