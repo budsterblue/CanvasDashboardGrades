8b789c767bdadfbbe9d023f2378e4f4480782a00
budsterblue <budsterblue@gmail.com>
2021-05-08 02:09:20 -0500

    massive rewrite that's fully commented and much easier to read

--- script.js
+++ script.js
@@ -1,52 +1,46 @@
-function onloadcheck() {
+// make sure the code is running on the dashboard page
 if (location.pathname == "/") {
-var courseGradesNum = [];
-var courseGradesID = [];
 
-//Get Grades
-fetch(location.protocol+'//'+location.host+location.pathname + 'api/v1/courses?include[]=total_scores&per_page=100&enrollment_state=active',{
+// fetch grades using the official rest api
+fetch(location.protocol + '//' + location.host + '/api/v1/courses?include[]=total_scores&per_page=100&enrollment_state=active', {
           method: 'GET',
           credentials: 'include',
           headers: {
                "Accept": "application/json"
           }
      })
-    .then(res => res.json())
+    .then(res => res.json()) // unused but necessary for some reason
     .then(data => obj = data)
     .then(function(){
 
-for(var i=0, len=obj.length; i<len; i++) {
-  var courseEnrollment = obj[i].enrollments[obj[i].enrollments.length -1].computed_current_score;
-  if (courseEnrollment == null){
-    courseEnrollment = "N/A";
-  } else {
-    courseEnrollment += "%";
-  }
-  courseGradesNum.push(courseEnrollment);
-  courseGradesID.push(obj[i].name);
+var courseGrades = new Map();
+
+// loop through all actively enrolled courses
+for (course of obj) {
+  // get a current course's grade
+  let courseGrade = course.enrollments[course.enrollments.length -1].computed_current_score;
+  // add course id and grade to map, adding a percent sign to grade or setting to N/A if null (no grade)
+  courseGrades.set(course.id, (courseGrade == null) ? "N/A" : courseGrade + "%");
 }
 
-//Dashboard Stuff
-var courseBase = document.getElementsByClassName('ic-DashboardCard__header_hero');
-for(var b=0, leng=courseBase.length; b<leng; b++) {
-  var courseName = document.getElementsByClassName('ic-DashboardCard__link')[b].children[0].children[0].title;
-  for(var a=0, lena=courseGradesID.length; a<lena; a++) {
-    if (courseName.includes(courseGradesID[a]) == true) {
-      var canvasTitle = document.createElement("h1");
-      canvasTitle.style.color = document.getElementsByClassName('ic-DashboardCard__header_hero')[b].style.backgroundColor;
-      canvasTitle.style.background = "white";
-      canvasTitle.style.borderRadius = "1em";
-      canvasTitle.style.display = "inline";
-      canvasTitle.style.padding = "5px 15px 5px 15px";
-      canvasTitle.style.lineHeight = "65px";
-      canvasTitle.style.marginLeft = "10px";
-      canvasTitle.style.fontSize = "150%";
-      var canvasTitleText = document.createTextNode(courseGradesNum[a]);
-      canvasTitle.appendChild(canvasTitleText);
-      document.getElementsByClassName('ic-DashboardCard__header_hero')[b].appendChild(canvasTitle);
-    }
+// only display grades once the page is loaded
+function onloadcheck() {
+  // display grades on the dashboard
+  for (elem of document.getElementsByClassName('ic-DashboardCard__header')) {
+    // get the element that will be the parent of the element we are about to create
+    let courseHeader = elem.querySelector('.ic-DashboardCard__header_hero');
+    // create the visible element
+    let gradeElem = document.createElement("h1");
+    // style the element
+    gradeElem.setAttribute("style", "color: " + courseHeader.style.backgroundColor + `; background: white; border-radius: 1em;
+      display: inline; padding: 5px 15px 5px 15px; line-height: 65px; margin-left: 10px; font-size: 150%;`);
+    // get course id from DOM and use it to pull the correct grade from the map
+    let courseID = parseInt(elem.querySelector('.ic-DashboardCard__link').href.split("/").pop());
+    gradeElem.innerHTML = courseGrades.get(courseID);
+    // add the element to the DOM
+    courseHeader.appendChild(gradeElem);
   }
 }
-});
-}}
 window.onload = onloadcheck;
+});
+}
